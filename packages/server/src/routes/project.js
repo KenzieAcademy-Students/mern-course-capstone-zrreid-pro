6601b7dd7f1896accedf85f3cb1444d7ca03f3d0
Nupogodi <contact.alexsherbin@gmail.com>
2021-09-15 10:37:50 -0400

    added basic pretested project routes for get, post and put requests. currently incomplete

--- packages/server/src/routes/project.js
+++ packages/server/src/routes/project.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const Project = require('../models/project');
-
+const requireAuth = require('../middleware/index');
 
 const router = express.Router();
 
+// @GET /api/project:id - Private (retrieve a specific project)
+router.get('/:id', requireAuth, async (req, res) => {
+  try {
+    //* verify the parsed search parameters
+    const project = await Project.find({ _id: req.params.id });
 
-router.get('/:id', async (req,res) => {
-
-    try {
-
-        
-        
-    } catch (error) {
-
-        res.status(500).send('Server Error');
-        console.error(error.message);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).end();
     }
-
+  } catch (error) {
+    res.status(500).send('Server Error');
+    console.error(error.message);
+  }
 });
 
+// @POST /api/project - Private (create a new project)
+router.post('/', requireAuth, async (req, res, next) => {
+  try {
+    //* review this catch block and make adjustments
+  } catch (error) {
+    res.status(500).send('Server Error');
+    next(error);
+  }
+});
+
+// @PUT /api/project:id - Private (update a project)
+router.put('/:id', requireAuth, async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
 
 module.exports = router;
