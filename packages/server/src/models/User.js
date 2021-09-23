e4e151642802fb0c1651df763e0dc339fdd3b6ee
Zachary Reid <zrreid97@gmail.com>
2021-09-23 15:33:31 -0400

    Fixing Merge.

--- packages/server/src/models/User.js
+++ packages/server/src/models/User.js
@@ -1,31 +1,29 @@
 const mongoose = require('mongoose');
-
 const { ObjectId } = mongoose.Schema.Types;
 
 const userSchema = new mongoose.Schema({
-  username: {
-      type: String,
-      unique: true,
-      required: true
-  },
-  passwordHash: {
-      type: String,
-      required: true
-  },
-  email: {
-      type: String,
-      required: true
-  },
-  avatar: [ Number ],
-  project_list: [
-      {
-          type: ObjectId,
-          ref: 'Project'
-      }
-  ]
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avatar: [ Number ],
+    project_list: [
+        {
+            type: ObjectId,
+            ref: 'Project'
+        }
+    ]
 });
 
 const User = mongoose.model('User', userSchema);
-
 
 module.exports = User;
