55b446e6ee40c81d22df243fb06a54c01a861051
Zachary Reid <zrreid97@gmail.com>
2021-10-11 11:01:19 -0400

    few more changes.

--- packages/server/src/db/users2.js
+++ packages/server/src/db/users2.js
@@ -1,22 +1,22 @@
 export const users2 = [
-    // {
-    //     username: 'CrimsonCoder',
-    //     password: 'password',
-    //     email: 'crimsoncoder@example.com',
-    //     avatar: { pattern: 0, color: '#DC143C' }
-    // },
+    {
+        username: 'CrimsonCoder',
+        password: 'password',
+        email: 'crimsoncoder@example.com',
+        avatar: { pattern: 0, color: '#DC143C' }
+    },
     {
         username: 'Neotazman',
         password: 'password',
         email: 'neotazman@example.com',
         avatar: { pattern: 0, color: '#8E8E38' }
     },
-    {
-        username: 'MeSherbsta',
-        password: 'password',
-        email: 'mesherbsta@example.com',
-        avatar: { pattern: 0, color: '#009ACD' }
-    },
+    // {
+    //     username: 'MeSherbsta',
+    //     password: 'password',
+    //     email: 'mesherbsta@example.com',
+    //     avatar: { pattern: 0, color: '#009ACD' }
+    // },
     {
         username: 'TreyThisOn4Size',
         password: 'password',
