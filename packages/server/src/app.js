02b3519881b599f8c86511bc8c82ed589d243e0f
Zachary Reid <zrreid97@gmail.com>
2021-10-13 16:42:41 -0400

    Deployment setup.

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -53,6 +53,13 @@ app.use(express.static(path.join(__dirname, '../../client/build')));
 // api router
 app.use(keys.app.apiEndpoint, router);
 
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  });
+}
+
+
 // catch 404 and forward to error handler
 // app.use((req, res, next) => {
 //   next(createError(404, 'NotFound'));
--- packages/server/src/config/keys.js
+++ packages/server/src/config/keys.js
@@ -4,7 +4,7 @@ module.exports = {
     apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
   },
   database: {
-    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/MERNApp', // for local mongodb ***put connection string here
+    url: process.env.MONGODB_URI || 'mongodb+srv://database-user:kenzie123@crimsoncoding.q97ys.mongodb.net/TaskMaster?retryWrites=true&w=majority', // for local mongodb ***put connection string here
     // url: "PUT-YOUR-MONGODB-CONNECTION-STRING-HERE" // for mongodb atlas, comment the above line, uncomment this line, and add your connection string.
   },
   jwt: {
